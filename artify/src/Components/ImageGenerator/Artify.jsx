448d6f7798919c559e1e8f158d682366aaeb214d
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-04 22:33:42 +0530

    Update Artify.jsx

--- artify/src/Components/ImageGenerator/Artify.jsx
+++ artify/src/Components/ImageGenerator/Artify.jsx
@@ -11,6 +11,7 @@ function Artify() {
         if (inputRef.current.value==="") {
             return 0;
         }
+        setLoading(true);
         const response = await fetch(
             "https://api.openai.com/v1/images/generations",
             {
@@ -29,6 +30,7 @@ function Artify() {
         let data = await response.json();
         let data_array = data.data;
         setImage_url(data_array[0].url);
+        setLoading(false);
     }
 
   return (
