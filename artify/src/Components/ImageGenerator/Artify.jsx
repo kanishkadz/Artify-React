b48dcad1984f5e3187fa5220abe69f185b28148a
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-04 21:57:54 +0530

    commit4

--- artify/src/Components/ImageGenerator/Artify.jsx
+++ artify/src/Components/ImageGenerator/Artify.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Artify.css';
+import default_image from '../Assets/default_image.svg'
 
 function Artify() {
   return (
-    <div></div>
+    <div className='artify'>
+        <div className="header">
+            <span>Artify</span>, AI image generator
+        </div>
+        <div className="img-loading">
+            <div className="image">
+                <img src={default_image} alt="" />
+            </div>
+        </div>
+
+    </div>
   )
 }
 
